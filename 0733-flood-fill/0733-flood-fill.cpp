d591791386c17f47ddb80313064e18fc7d133107
Tejashri Choudhary <tejashrichoudhary2@gmail.com>
2026-08-30 14:10:18 +0530

    Time: 4 ms (5.46%), Space: 18.1 MB (74.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public: 
-    int initialclr;
-    void dfs (int row,int col,int color,vector<vector<int>>& ans, vector<vector<int>>& image,int dirrow[],int dircol[]){
-      ans[row][col] = color;
-      int n = image.size();
-      int m  = image[0].size();
-      for(int i = 0; i < 4; i++){
-        int nrow = row+dirrow[i];
-        int ncol = col+dircol[i];
-        if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol]==initialclr && ans[nrow][ncol] != color){
-            dfs(nrow,ncol,color,ans,image,dirrow,dircol);
+    int V,E;
+    void dfs(int row,int col ,vector<vector<int>>& image,int originalcolour, int color,vector<vector<bool>>& visited){
+        visited[row][col] = true;
+        image[row][col ] = color;
 
+        int dr[4] = {-1,1,0,0};
+        int dc[4]= {0,0,-1,1};
+
+        for(int k = 0; k < 4; k++){
+            int newrow = row + dr[k];
+            int newcol = col + dc[k];
+        
+        if(newrow >= 0 && newrow < V && newcol >= 0 && newcol <E){
+            
+            if(image[newrow][newcol] == originalcolour && !visited[newrow][newcol]){
+                 dfs(newrow, newcol,image,originalcolour,color,visited);
+            }
+            }
         }
-      }
-
-
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-         initialclr = image[sr][sc];
-        vector<vector<int>> ans = image;
-        int dirrow[] ={-1,0,1,0};
-        int dircol[] ={0,+1,0,-1};
-        dfs(sr,sc,color,ans,image,dirrow,dircol);
-        return ans;
+        V =image.size();
+         E = image[0].size();
+        int originalcolour = image[sr][sc];
+        vector<vector<bool >> visited(V,vector<bool>(E,false));
+    
+        if(originalcolour == color)
+        return image;
+        dfs(sr,sc,image,originalcolour,color,visited);
+        return image;
+         
     }
 };
