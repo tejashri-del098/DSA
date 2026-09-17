0b40c281b180918180d96fd45441e4a2c74a11cb
Tejashri Choudhary <tejashrichoudhary2@gmail.com>
2026-09-17 10:51:27 +0530

    Time: 0 ms (100%), Space: 13.3 MB (43.97%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,25 +1,64 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        /*int n = matrix.size();
+        int m = matrix[0].size();
+        int i = 0;
+        int j = m-1;
+        while(i < n && j >= 0){
+            if(matrix[i][j]== target){
+                return true;
+            }else if (matrix[i][j] > target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return false;*/
+        /*int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int>arr;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                arr.push_back(matrix[i][j]);
+            }
+        }
+        int left = 0;
+        int right = arr.size()-1;
+        while(left <= right){
+            int mid = left + (right- left)/2;
+            if(arr[mid] == target){
+                return true;
+            }
+            else if(arr[mid] > target){
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return false;*/
         int n = matrix.size();
         int m = matrix[0].size();
-        int i =0;
-        int left = 0;
-        int right = n-1;
-        for(int j = n-1; j>= 0; j--){
-           if(matrix[i][j] > target){
-             int mid = left+(right-left)/2;
-             if(mid == target){
-                return true;
-             }else if(mid > target){
-                right--;
-             }else{
-                left++;
-             }
 
-           }
-           return true;
+        int left = 0;
+        int right = n*m-1;
+
+        while(left <= right){
+            int mid = left +(right-left)/2;
+            int row = mid/m;
+            int col = mid%m;
+
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                right = mid -1;
+            }else{
+                left = mid+1;
+            }
         }
         return false;
+         
+            
     }
 };
